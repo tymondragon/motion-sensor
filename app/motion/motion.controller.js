4c1c2d3f06b2f2ad11793558af0486c494e098cd
Ty Mondragon <tymondragon@gmail.com>
2019-10-23 21:59:52 -0600

    guessing game is finito

--- app/motion/motion.controller.js
+++ app/motion/motion.controller.js
@@ -9,8 +9,8 @@ const direction = {
 
 exports.useMotion = async (req, res, next) => {
   deviceManager.listConnectedDevices()
-  .then((devices) => {
-    let count = 0;
+    .then((devices) => {
+      let count = 0;
       let msk = devices.find((device) => {
         return device instanceof MotionSensorKit;
       });
@@ -18,23 +18,48 @@ exports.useMotion = async (req, res, next) => {
         console.log('No Motion Sensor Kit was found :(');
       } else {
         console.log('Motion Sensor Kit found!');
-        msk.setMode('gesture')
-        .then(() => {
-            console.log('Set to `gesture` mode. Try to see if you can guess which direction is correct 5 times as fast as you can. Move your hand up, down, left, or right.')
+        setProximity(msk)
+
+        msk.on('proximity', (p) => {
+          if (p > 0) {
+            console.log('Proximity value:', p)
+          }
+          if (p > 250) {
+            setGesture(msk)
+          }
         });
-        // Prints gesture data
+
         msk.on('gesture', (g) => {
           let randomDirection = direction[`${Math.floor(Math.random() * 3)}`]
-          if (g === randomDirection){
-            count++
+          if (g === randomDirection) {
             console.log(`
-            Nice! you selected, ${randomDirection} correctly!
-            ${5 - count} to go
-            `)
+            Nice! you selected, ${randomDirection} correctly! The game will restart in 3 seconds`)
+            setTimeout(() => setProximity(msk), 3000)
+            count = 0;
           } else {
-            console.log(`Sorry, ${randomDirection} was the correct direction. Keep trying!`)
+            count++
+            console.log(`Nuts!!! ${randomDirection} was the correct direction, you chose ${g}. Keep trying!`)
+          }
+          if (count === 3) {
+            console.log(`Oooh, too bad that was your last guess!!! You chose ${g} but ${randomDirection} was the correct one. The game will start again in 3 seconds`)
+            setTimeout(() => setProximity(msk), 3000)
+            count = 0;
           }
         });
       }
     })
+}
+
+function setProximity(msk) {
+  msk.setMode('proximity')
+    .then(() => {
+      console.log('Get 3 chances to guess the direction: Up, Down, Left, Right. To start the game lower your hand until the promity value is > than 250')
+    });
+}
+
+function setGesture(msk) {
+  msk.setMode('gesture')
+    .then(() => {
+      console.log('Let the games begin!!!')
+    });
 }
